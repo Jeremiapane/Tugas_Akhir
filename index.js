12452c1b697af3e45383c6efb7bb48ee7229aaf1
Jeremia Pane <jeripane@gmail.com>
2022-07-23 21:04:21 +0700

    create func for navbar when scrolling

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const burgerBtn = document.querySelector('#burgerBtn');
 const navLink = document.querySelector(".nav-links");
 
+
 burgerBtn.addEventListener('click', () => {
-    navLink.classList.toggle('expand');
+    navLink.classList.toggle('expand')
 });
+
+window.addEventListener('scroll', () => {
+    navLink.classList.remove('expand')
+    // let navbar = document.querySelector('nav');
+    // let windowPosition = window.scrollY > 0;
+    // navbar.classList.toggle('scrolling-active', windowPosition)
+})
+
+
 
 
